5dc96f69d7f6f796a306a3b74114ab203b9e3bf2
Sujay Mishra <sujay.nandan23@gmail.com>
2026-09-11 22:51:26 +0530

    Time: 0 ms (100%), Space: 8.6 MB (92.52%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    string roman(int n) {
+        switch(n) {
+            case 1000: return "M";
+            case 900:  return "CM";
+            case 500:  return "D";
+            case 400:  return "CD";
+            case 100:  return "C";
+            case 90:   return "XC";
+            case 50:   return "L";
+            case 40:   return "XL";
+            case 10:   return "X";
+            case 9:    return "IX";
+            case 5:    return "V";
+            case 4:    return "IV";
+            case 1:    return "I";
+        }
+
+        return "";
+    }
+
+    string intToRoman(int num) {
+
+        string ans = "";
+
+        int values[] = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        for(int i = 0; i < 13; i++) {
+
+            while(num >= values[i]) {
+                ans += roman(values[i]);
+                num -= values[i];
+            }
+        }
+
+        return ans;
+    }
+};
